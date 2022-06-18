69cc04484de8fd7d63f697c663122dc8247e5392
LucyKor <76470351+LucyKor@users.noreply.github.com>
2022-06-18 21:32:19 +0200

    Add files via upload

---
+++ VypisMesicu.py
@@ -0,0 +1,5 @@
+mesice = ["leden", "únor", "březen", "duben", "květen", "červen", "červenec", "srpen", "září", "říjen", "listopad", "prosinec"]
+
+print("Seznam měsíců:")
+for i in range(12):
+    print(mesice[i])
